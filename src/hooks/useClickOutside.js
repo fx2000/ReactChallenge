bacba569166448a2b73931e88b0e94222396c142
Daniel Duque <duque@outlook.com>
2022-05-09 18:27:21 -0500

    Updated custom hooks

--- src/hooks/useClickOutside.js
+++ src/hooks/useClickOutside.js
@@ -9,12 +9,12 @@ export const useClickOutside = (ref, handler) => {
       handler(event)
     }
 
-    document.addEventListener("mousedown", listener)
-    document.addEventListener("touchstart", listener)
+    document.addEventListener('mousedown', listener)
+    document.addEventListener('touchstart', listener)
 
     return () => {
-      document.removeEventListener("mousedown", listener)
-      document.removeEventListener("touchstart", listener)
+      document.removeEventListener('mousedown', listener)
+      document.removeEventListener('touchstart', listener)
     }
   }, [ref, handler])
 }
